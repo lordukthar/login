eb8ac058aac3bcbb588180009ad81106b51f5bb0
Jonas A <jonas@lhost.se>
2024-12-01 16:55:53 +0100

    wip 1

---
+++ src/app/models/item.interface.ts
@@ -0,0 +1,7 @@
+// src/app/models/item.interface.ts
+export interface Item {
+  href: string | undefined;
+  name: string | undefined;
+  price: number | undefined;
+  description: string | undefined;
+}
--- src/app/object-list/object-list.component.ts
+++ src/app/object-list/object-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Item } from '../models/item.interface';
 
 @Component({
   selector: 'app-object-list',
@@ -10,11 +11,26 @@ export class ObjectListComponent {
   @Output() itemDeleted = new EventEmitter<number>();
   @Output() itemEdited = new EventEmitter<{ index: number; item: any }>();
 
+   @Input() items_i: Item[] = [];
+    @Output() itemDeleted_i = new EventEmitter<number>();
+    @Output() itemEdited_i = new EventEmitter<{ index: number; item: Item }>();
+
   isEditing = false;
+    newItem = { href: '', name: '', price: 0, description: '' };
   editedItem: any = null;
   editingIndex: number | null = null;
+   isAddFormVisible = false; // Initially hidden
+
+
+    toggleAddForm() {
+       this.isAddFormVisible = !this.isAddFormVisible;
+     }
 
   deleteItem(index: number) {
+    this.itemDeleted_i.emit(index); // Notify parent component to handle deletion
+  }
+
+ deleteItemI(index: number) {
     this.itemDeleted.emit(index); // Notify parent component to handle deletion
   }
 
@@ -22,6 +38,20 @@ export class ObjectListComponent {
     this.isEditing = true;
     this.editingIndex = index;
     this.editedItem = { ...this.items[index] }; // Make a copy of the item to edit
+  }
+
+  editItemI(index: number) {
+      this.isEditing = true;
+      this.editingIndex = index;
+      this.editedItem = { ...this.items_i[index] }; // Make a copy of the item to edit
+    }
+
+  saveChangesI() {
+    if (this.editingIndex !== null) {
+      this.items_i[this.editingIndex] = { ...this.editedItem }; // Save the changes to the item
+      this.isEditing = false; // Hide the modal after saving
+      this.editingIndex = null;
+    }
   }
 
   saveChanges() {
@@ -32,8 +62,19 @@ export class ObjectListComponent {
     }
   }
 
+  cancelEditI() {
+      this.isEditing = false; // Hide the modal without saving changes
+      this.editingIndex = null;
+    }
+
   cancelEdit() {
     this.isEditing = false; // Hide the modal without saving changes
     this.editingIndex = null;
   }
+
+  addItem() {
+      this.items.push({ ...this.newItem });
+      this.newItem = { href: '', name: '', price: 0, description: '' };
+      this.isAddFormVisible = false; // Hide the form after adding an item
+    }
 }
