4776fa70719fb9a3bde0341601fa81d5d6d802da
Jonas A <jonas@lhost.se>
2024-12-05 17:01:43 +0100

    wip 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,153 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { IntervalEmitterService } from './interval-emitter.service';
 import { concat, concatMap, delay, exhaustMap, interval, mergeMap, of, switchMap, take, zip } from 'rxjs';
 
+import { initializeApp, FirebaseApp } from "firebase/app";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-zip() {
-  const observable1 = interval(1000).pipe(take(6)); // Emits 0, 1, 2
+export class AppComponent  {
 
-  // Observable 2: emits 'A', 'B', 'C'
-  const observable2 = of('A', 'B', 'C', 'D','E', 'F');
+   firebaseConfig = {
+     apiKey: "AIzaSyBedWTSjkMDR9Xlc1wP8A5U1f88du_CILE",
+     authDomain: "christmas-c6610.firebaseapp.com",
+     projectId: "christmas-c6610",
+     storageBucket: "christmas-c6610.appspot.com",
+     messagingSenderId: "473969574069",
+     appId: "1:473969574069:web:abd9b5a2673aa20c2cbefc",
+     measurementId: "G-0S3YEM15PR",
+   };
 
-  zip(observable1, observable2).subscribe(([num, letter]) => {
-    this.zipMapValues.push(`Number: ${num}, Letter: ${letter}`)
-    console.log(`Number: ${num}, Letter: ${letter}`);
-  });
-  
-}
-  zipMapValues: string[] = [];
-  exhaustMapValues: number[] = [];
-  switchMapValues: number[] = [];
-  mergeMapValues: number[] = [];
-  concatMapValues: number[] = [];
-
-exhaust() {
-  interval(500).pipe(
-    take(10), // Only take the first 10 values (0, 1, 2, ... 9)
-    exhaustMap(value => {
-      // Simulate processing of each value with a delay of 1000ms
-      this.exhaustMapValues.push(value);
-      return of(`Processed ${value}`).pipe(delay(1000));
-    })
-  ).subscribe(result => {
-    console.log(result);
-  });
-}
-
-switch() {
-  interval(1000).pipe(
-    take(5), // Take the first 5 values (0, 1, 2, 3, 4)
-    switchMap(value => {
-      // Simulate processing
-      return of(value).pipe(
-        delay(1000), // Simulate latency
-        switchMap(val => {
-          // Log the value
-          console.log(`Logged: ${val}`);
-          
-          // Add the value to the loggedValues array
-          this.switchMapValues.push(val);
-          
-          // Continue with the stream
-          return of(val);
-        })
-      );
-    })
-  ).subscribe(() => {
-    // Optionally, you can perform actions when each value is processed
-  });
-  
-  // After some time or when needed, you can access the loggedValues array
-  setTimeout(() => {
-    console.log('All logged values:', this.switchMapValues);
-  }, 6000); // Wait a bit longer than the total processing time
-  
-
-}
-
-concat() {
-  of(1, 2, 3).pipe(
-    concatMap(value => {
-      this.concatMapValues.push(value);
-      return of(`Processed ${value}`).pipe(delay(1000));
-    })
-  ).subscribe(result => {
-    console.log("concatMap: ", result);
-  });
-}
-
-  merge(){
-    interval(1000).pipe(
-      take(5), // Take the first 5 values (0, 1, 2, 3, 4)
-      mergeMap(value => {
-        // Simulate processing
-        const randomDelay = Math.floor(Math.random() * 3000); 
-
-        return of(value).pipe(
-          delay(randomDelay), // Simulate latency
-          mergeMap(val => {
-            // Log the value
-            console.log(`Logged: ${val}`);
-            
-            // Add the value to the loggedValues array
-            this.mergeMapValues.push(val);
-            
-            // Continue with the stream
-            return of(val);
-          })
-        );
-      })
-    ).subscribe(() => {
-      // Optionally, you can perform actions when each value is processed
-    });
-
-
-
-  }
 
   title = 'my-app';
 
-  constructor(private intervalEmitterService: IntervalEmitterService) {}
 
-  ngOnInit() {
-    this.intervalEmitterService.getExhaustMap().subscribe(val => {
-      //this.exhaustMapValues.push(val);
-    });
-
-    this.intervalEmitterService.getMergeMap().subscribe(val => {
-      //this.mergeMapValues.push(val);
-    });
-
-    this.intervalEmitterService.getSwitchMap().subscribe(val => {
-     // this.switchMapValues.push(val);
-    });
-
-    this.intervalEmitterService.getConcatMap().subscribe(val => {
-      //this.concatMapValues.push(val);
-    });
-
-
-    const lambdaFunction = (name: string) => name;
-    const processedLambda = this.processNameLambda(lambdaFunction);
-    const result = processedLambda('angular');
-    console.log(result); 
-  }
-
-
-
-  processNameLambda(lambda: (name:string) => string) {
-    return (name:string) => {
-      const processName = name.toUpperCase();
-      return {
-        name:processName,
-        num: processName.length
-      }
-    }
-  }
-
+    app = initializeApp(this.firebaseConfig);
 
 }
--- src/app/session.service.ts
+++ src/app/session.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import { Observable, throwError, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+
+
+interface User {
+  username: string;
+  password: string; // Storing plain text passwords is not secure; ensure you understand the risks.
+  sessionToken?: number;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -10,19 +17,46 @@ export class SessionService {
 
   private apiUrl = 'http://localhost:5000';
 
+
+   users: User[] = [
+    { username: "Jenny", password: "fotboll", },
+    { username: "Otto", password: "otto1234" },
+    { username: "Jonas", password: "Pkurlips1" },
+    { username: "Sonia", password: "Shiraz1349" },
+  ];
+
+
+  findLoggedInUser = (username: string, password: string): User | null => {
+
+   let  us : User | null =  this.users.find((user:User) => user.username === username && user.password === password) || null;
+
+   if (us) {
+        let modUs:User = {
+               ...us,
+               sessionToken: Date.now()}
+
+
+               console.log("FOO ", JSON.stringify(modUs));
+
+               return modUs;
+       }
+
+
+
+    return us;
+  };
+
   constructor(private http: HttpClient) { }
 
 login(username: string, password: string): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/login`, { username, password }).pipe(
-        catchError(this.handleError)
-      )
+    return of(this.findLoggedInUser(username, password));
   }
 
   getSession(sessionToken: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/session`, { params: { sessionToken } })
-     .pipe(
-            catchError(this.handleError)
-          );
+
+
+   return of(localStorage.getItem("sessionToken"));
+
   }
 
 private handleError(error: HttpErrorResponse) {
